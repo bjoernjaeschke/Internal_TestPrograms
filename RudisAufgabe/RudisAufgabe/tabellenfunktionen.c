c543607a59bc6fc7191059fbd89e39eecb9a0f7c
Björn Jaeschke <bjoern.jaeschke@soehnle-professional.com>
2020-03-05 10:47:44 +0100

    Added a file for the newly required functions.

---
+++ RudisAufgabe/RudisAufgabe/tabellenfunktionen.c
@@ -0,0 +1,30 @@
+int f0(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f1(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f2(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f3(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f4(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f5(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f6(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f7(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f8(File * pOutputFile, char * pParameter) {
+	return 0;
+}
+int f9(File * pOutputFile, char * pParameter) {
+	return 0;
+}
